091e5c3e6639471bfa2dba233eb5e8ae25700fb0
Kevin Hellemun <kevin@messagebird.com>
2019-02-18 19:25:50 +0100

    init dep.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=project-b-backend
 docker build --build-arg TAG=${TRAVIS_COMMIT} -t ${PROJECT_NAME} -f ./cmd/http/Dockerfile .
 
 docker tag ${PROJECT_NAME} ${PROJECT_NAME}:latest
-docker tag $1:latest ogkevin/${PROJECT_NAME}:latest
+docker tag ${PROJECT_NAME}:latest ogkevin/${PROJECT_NAME}:latest
 
 docker tag ${PROJECT_NAME} ${PROJECT_NAME}:${TRAVIS_COMMIT}
 docker tag ${PROJECT_NAME}:${TRAVIS_COMMIT} ogkevin/${PROJECT_NAME}:${TRAVIS_COMMIT}
