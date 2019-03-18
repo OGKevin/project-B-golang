815a3de6e0dec8c505bfdec35031fa250f4d73a5
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2019-03-18 14:27:11 +0100

    Remove uneeded comments.

--- interal/acl/acl.go
+++ interal/acl/acl.go
@@ -6,24 +6,5 @@ import (
 )
 
 func NewEnforcer(a persist.Adapter) *casbin.Enforcer {
-	// Or you can use an existing DB "abc" like this:
-	// The adapter will use the table named "casbin_rule".
-	// If it doesn't exist, the adapter will create it automatically.
-	// a := xormadapter.NewAdapter("mysql", "mysql_username:mysql_password@tcp(127.0.0.1:3306)/abc", true)
-
 	 return casbin.NewEnforcer(casbin.NewModel(conf), a)
-	//
-	//// Load the policy from DB.
-	//e.LoadPolicy()
-	//
-	//// Check the permission.
-	//e.Enforce("alice", "data1", "read")
-	//e.AddPolicy()
-	//
-	//// Modify the policy.
-	//// e.AddPolicy(...)
-	//// e.RemovePolicy(...)
-	//
-	//// Save the policy back to DB.
-	//e.SavePolicy()
 }
