04c1bc77ec0437ed48c2ed94d281b7a523307cc6
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2019-03-25 00:19:53 -0400

    Implement create and get method.

--- interal/coordinates/repository.go
+++ interal/coordinates/repository.go
@@ -3,17 +3,22 @@ package coordinates
 import (
 	"github.com/jmoiron/sqlx"
 	"github.com/paulmach/go.geo"
+	"github.com/pkg/errors"
 	"github.com/satori/go.uuid"
 )
 
 type Point struct {
-	ID uuid.UUID
-	UserID uuid.UUID
+	ID          uuid.UUID
+	UserID      uuid.UUID
 	Coordinates *geo.Point
 }
 
+func NewPoint(userID uuid.UUID, coordinates *geo.Point) *Point {
+	return &Point{UserID: userID, Coordinates: coordinates}
+}
+
 type coordinates interface {
-	Create(point *geo.Point) (uuid.UUID, error)
+	Create(point *Point) (uuid.UUID, error)
 	Get(ID uuid.UUID) (Point, error)
 	ListByUserID(userID uuid.UUID) (chan Point, error)
 	Delete(ID uuid.UUID) error
@@ -27,18 +32,40 @@ func NewCoordinatesFromDatabase(db *sqlx.DB) *coordinatesFromDatabase {
 	return &coordinatesFromDatabase{db: db}
 }
 
-func (coordinatesFromDatabase) Create(point *geo.Point) (uuid.UUID, error) {
+func (c *coordinatesFromDatabase) Create(point *Point) (uuid.UUID, error) {
+	id := uuid.NewV4()
+	_, err := c.db.Exec(`insert into coordinates (id, user_id, point) value (?, ?, geometryfromtext(?))`, id, point.UserID, point.Coordinates.ToWKT())
+	if err != nil {
+		return uuid.Nil, errors.Wrap(err, "could not save coordinates in database")
+	}
+
+	return id, nil
+}
+
+func (c *coordinatesFromDatabase) Get(ID uuid.UUID) (*Point, error) {
+	rows, err := c.db.Query(`select user_id, point from coordinates where id := ? limit 1`, ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get point by id")
+	}
+
+	defer rows.Close()
+
+	var p Point
+
+	rows.Next()
+	if err = rows.Scan(&p.UserID, &p.Coordinates); err != nil {
+		return nil, errors.Wrap(err, "could not scan result for getting point by id")
+	}
+
+	p.ID = ID
+
+	return &p, nil
+}
+
+func (c *coordinatesFromDatabase) ListByUserID(userID uuid.UUID) (chan Point, error) {
 	panic("implement me")
 }
 
-func (coordinatesFromDatabase) Get(ID uuid.UUID) (Point, error) {
-	panic("implement me")
-}
-
-func (coordinatesFromDatabase) ListByUserID(userID uuid.UUID) (chan Point, error) {
-	panic("implement me")
-}
-
-func (coordinatesFromDatabase) Delete(ID uuid.UUID) error {
+func (c *coordinatesFromDatabase) Delete(ID uuid.UUID) error {
 	panic("implement me")
 }
